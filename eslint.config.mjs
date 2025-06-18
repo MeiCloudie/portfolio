5ea39fa42092a17b5c1c4aeb76be0860a283f8b4
Truong Thuc Van <truongthucvan1242@gmail.com>
2025-06-18 17:09:47 +0700

    setup(code-style): Prettier, Eslint - use CLI to synchronously change the code format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,38 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  {
+    ignores: ['node_modules/**', '.next/**', 'build/**', 'public/**', 'dist/**', 'coverage/**']
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  ...compat.plugins('prettier'),
+  {
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ]
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
--- src/components/ui/text-generate-effect.tsx
+++ src/components/ui/text-generate-effect.tsx
@@ -1,13 +1,13 @@
-"use client"
-import { useEffect } from "react"
-import { motion, stagger, useAnimate } from "motion/react"
-import { cn } from "@/lib/utils"
+'use client'
+import { useEffect } from 'react'
+import { motion, stagger, useAnimate } from 'motion/react'
+import { cn } from '@/lib/utils'
 
 export const TextGenerateEffect = ({
   words,
   className,
   filter = true,
-  duration = 0.5,
+  duration = 0.5
 }: {
   words: string
   className?: string
@@ -15,19 +15,20 @@ export const TextGenerateEffect = ({
   duration?: number
 }) => {
   const [scope, animate] = useAnimate()
-  let wordsArray = words.split(" ")
+  const wordsArray = words.split(' ')
   useEffect(() => {
     animate(
-      "span",
+      'span',
       {
         opacity: 1,
-        filter: filter ? "blur(0px)" : "none",
+        filter: filter ? 'blur(0px)' : 'none'
       },
       {
         duration: duration ? duration : 1,
-        delay: stagger(0.2),
+        delay: stagger(0.2)
       }
     )
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [scope.current])
 
   const renderWords = () => {
@@ -37,12 +38,12 @@ export const TextGenerateEffect = ({
           return (
             <motion.span
               key={word + idx}
-              className="dark:text-white text-black opacity-0"
+              className='dark:text-white text-black opacity-0'
               style={{
-                filter: filter ? "blur(10px)" : "none",
+                filter: filter ? 'blur(10px)' : 'none'
               }}
             >
-              {word}{" "}
+              {word}{' '}
             </motion.span>
           )
         })}
@@ -51,11 +52,9 @@ export const TextGenerateEffect = ({
   }
 
   return (
-    <div className={cn("font-bold", className)}>
-      <div className="mt-4">
-        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
-          {renderWords()}
-        </div>
+    <div className={cn('font-bold', className)}>
+      <div className='mt-4'>
+        <div className=' dark:text-white text-black text-2xl leading-snug tracking-wide'>{renderWords()}</div>
       </div>
     </div>
   )
